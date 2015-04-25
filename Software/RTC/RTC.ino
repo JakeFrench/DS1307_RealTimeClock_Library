2544a4909e870281fbbcafcc58d46debce5cdc6c
Jake French <jake.french@micronicsltd.co.uk>
2015-04-25 22:43:25 +0100

    Code base completed

--- Software/RTC/RTC.ino
+++ Software/RTC/RTC.ino
@@ -1,7 +1,11 @@
-#include "stdafx.h"
-#include "stdlib.h"
-#include "stdint.h"
-using namespace std;
+/*
+ * RTC.ino
+ * // Internal vars [ISO format used: 15-03-22T12:26:53Z]
+ * char input[] = { '1', '5', '-', '0', '3', '-', '2', '2', 'T', '1', '2', ':', '2', '6', ':', '5', '3', 'Z' };
+ *
+ * Created: 4/25/2015 9:31:14 PM
+ * Author: Dr Jake French
+ */ 
 
 // Internal vars [ISO format used: 15-03-22T12:26:53Z]
 char input[] = { '1', '5', '-', '0', '3', '-', '2', '2', 'T', '1', '2', ':', '2', '6', ':', '5', '3', 'Z' };
@@ -10,16 +14,15 @@ char input[] = { '1', '5', '-', '0', '3', '-', '2', '2', 'T', '1', '2', ':', '2'
 class DateTime{
 	public:
 		DateTime(uint8_t Address);
-		bool	processState(char const ch);
 		void	setDateTime();
 		uint8_t *getDateTime()							{ return &dateTimeBuffer[0]; }
+			
 	private:
+		bool	processState(char const ch);
+		void    readDateTime();
 		uint8_t bcdToDec(uint8_t const &val)			{ return ((val / 16 * 10) + (val % 16)); }
 		uint8_t decToBcd(uint8_t const &val)			{ return ((val / 10 * 16) + (val % 10)); }
-		uint8_t year, month, day, hour, minute, second;
-		uint8_t dateTimeBuffer[7];
-		uint8_t index;
-		uint8_t DS1307_ADDRESS;
+		uint8_t DS1307_ADDRESS, index, dateTimeBuffer[7];
 		char chInput[2];
 		enum
 		{
@@ -32,27 +35,33 @@ class DateTime{
 		} CheckSate;
 };
 
-
 DateTime::DateTime(uint8_t Address){
-	// basic configuration, as an emergency use basic[] to set the date & time   
+	// basic configuration, as an emergency use basic[] to set the date & time
 	DS1307_ADDRESS = Address;
-	year = month = day = hour = minute = second = 0;
-	uint8_t tmp[7] = { year, month, day, 7, hour, minute, second };
-	for(uint8_t i=0; i<7; i++)
-		dateTimeBuffer[i] = tmp[i];
-	index     = 0;
-	CheckSate = CHECK_YEAR;
-} 
+	CheckSate      = CHECK_YEAR;
+	index          = 0;
+	// read RTC data from the clock and populate the dateTimeBuffer[] array
+	readDateTime();
+}
 
+void DateTime::readDateTime(){
+	for(uint8_t i=0; i<7; i++)
+		dateTimeBuffer[i] = 1;
+}
 
 bool DateTime::processState(char const ch){
+	// interfacing with RTC over I2C is in this format (reverse ISO order):
+	//						 ss(0-59), mm(0-59), hh(0-23), DayOfWeek(not used), DD(1-31), MM(1-12), YY(00-99)
+	// dateTimeBuffer[7] = { second,   minute,   hour,           7,              day,      month,    year }
 	bool    success = false;
 	uint8_t number  = 0;
+	
 	// ensure the char is a number
 	if((ch >= '0') && (ch <= '9'))
 		chInput[index++] = ch;
 	else
 		index = 0;
+	
 	// now process the chInput buffer
 	if (index == 2){
 		index = 0;
@@ -60,7 +69,7 @@ bool DateTime::processState(char const ch){
 		switch (CheckSate){
 			case CHECK_YEAR:
 				if ((number >= 0) && (number <= 99)){
-					year = number;
+					dateTimeBuffer[6] = number; // set year 
 					CheckSate  = CHECK_MONTH;
 				}
 				else
@@ -68,7 +77,7 @@ bool DateTime::processState(char const ch){
 				break;
 			case CHECK_MONTH:
 				if ((number >= 1) && (number <= 12)){
-					month = number;
+					dateTimeBuffer[5] = number; // set month
 					CheckSate   = CHECK_DAY;
 				}
 				else
@@ -76,7 +85,7 @@ bool DateTime::processState(char const ch){
 				break;
 			case CHECK_DAY:
 				if ((number >= 1) && (number <= 31)){
-					day = number;
+					dateTimeBuffer[4] = number; // set day
 					CheckSate = CHECK_HOUR;
 				}
 				else
@@ -84,24 +93,24 @@ bool DateTime::processState(char const ch){
 				break;
 			case CHECK_HOUR:
 				if ((number >= 0) && (number <= 23)){
-					hour = number;
-					CheckSate  = CHECK_MINUTE;
+					dateTimeBuffer[2] = number;  // set hour
+					CheckSate = CHECK_MINUTE;
 				}
 				else
 					CheckSate = CHECK_YEAR;
 				break;
 			case CHECK_MINUTE:
 				if ((number >= 0) && (number <= 59)){
-					minute = number;
-					CheckSate    = CHECK_SECOND;
+					dateTimeBuffer[1] = number; // set minute
+					CheckSate = CHECK_SECOND;
 				}
 				else
 					CheckSate = CHECK_YEAR;
 				break;
 			case CHECK_SECOND:
 				if ((number >= 0) && (number <= 59)){
-					success      = true;
-					this->second = number;
+					success = true;
+					dateTimeBuffer[0]  = number; // set seconds
 				}
 				CheckSate = CHECK_YEAR;
 				break;
@@ -114,32 +123,29 @@ bool DateTime::processState(char const ch){
 
 
 void DateTime::setDateTime(){
-	// interfacing with RTC over I2C is in this format (reverse ISO order):
-	//				 ss(0-59), mm(0-59), hh(0-23), DayOfWeek(not used), DD(1-31), MM(1-12), YY(00-99) 
-	uint8_t tmp[] = { second,  minute,   hour,     7,                   day,      month,    year };
-	
 	//Wire.beginTransmission(DS1307_ADDRESS);
-	//Wire.write(0x00);						// stop Oscillator
-	for (int i = 0; i < 7; i++){
-		dateTimeBuffer[i] = tmp[i];
-		//Wire.write(decToBcd(tmp[i]));
-	}
-	//Wire.write(0x00);                       // start Oscillator
+	//Wire.write(0x00);								// stop Oscillator
+	//for (int i = 0; i < 7; i++){					// send buffer one byte at a time 
+	//	Wire.write(decToBcd(dateTimeBuffer[i]));
+	//}
+	//Wire.write(0x00);								// start Oscillator
 	//Wire.endTransmission();
 }
 
 
 
+DateTime RTC(0x68);
 
-int _tmain(int argc, _TCHAR* argv[])
+void setup()
 {
-	DateTime RTC(0x68);
-	bool test = false;
-	for (int i = 0; i < 18; i++){
-		if (RTC.processState(input[i]))
-			RTC.setDateTime();
-	}
 
-	return 0;
+	  /* add setup code here, setup code runs once when the processor starts */
+
 }
 
+void loop()
+{
+
+	  /* add main program code here, this code starts again each time it ends */
+
+}
